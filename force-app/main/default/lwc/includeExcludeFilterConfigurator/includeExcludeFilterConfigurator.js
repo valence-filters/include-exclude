fac54ed01d38ecc691a7ad71b697b6f76f0b579b
Chuck Liddell <chuck@valence.app>
2026-03-12 19:03:04 -0400

    Add support for a “starts with” behavior

--- force-app/main/default/lwc/includeExcludeFilterConfigurator/includeExcludeFilterConfigurator.js
+++ force-app/main/default/lwc/includeExcludeFilterConfigurator/includeExcludeFilterConfigurator.js
@@ -2,18 +2,21 @@
  * UI plugin that helps the user configure the Include Exclude Valence Filter.
  */
 
-import ValenceUIConfigurator from 'c/valenceUIConfigurator';
+import ValenceUIConfigurator from "c/valenceUIConfigurator";
 
 export default class IncludeExcludeFilterConfigurator extends ValenceUIConfigurator {
-
-	conditionOptions = [{label : 'MUST', value : 'MUST'}, {label : 'MUST NOT', value : 'MUST NOT'}];
+	conditionOptions = [
+		{ label: "MUST", value: "MUST" },
+		{ label: "MUST NOT", value: "MUST NOT" },
+		{ label: "MUST START WITH", value: "MUST START WITH" },
+	];
 
 	// -------------------------------------------
 	// ----- User Manipulating Configuration -----
 	// -------------------------------------------
 
 	addValue() {
-		this.configuration.values.push('');
+		this.configuration.values.push("");
 		this.configUpdated(); // propagate our configuration changes
 	}
 
@@ -24,12 +27,12 @@ export default class IncludeExcludeFilterConfigurator extends ValenceUIConfigura
 
 	longDebounceValueUpdated(event) {
 		// we debounce so that the user isn't forced to click out of the field before they click save changes, but we use a long debounce so the field doesn't lose focus on re-render
-		const index = event.target.dataset.index, value = event.target.value;
+		const index = event.target.dataset.index,
+			value = event.target.value;
 		this.debounce(() => {
 			this.configuration.values[index] = value;
 			this.configUpdated(); // propagate our configuration changes
-			}, 2000
-		);
+		}, 2000);
 	}
 
 	valueUpdatedImmediately(event) {
@@ -43,11 +46,15 @@ export default class IncludeExcludeFilterConfigurator extends ValenceUIConfigura
 	// -----------------------------------------
 
 	getDefaultShape() {
-		return {condition : 'MUST', values : [], ignoreReason : ''};
+		return { condition: "MUST", values: [], ignoreReason: "" };
 	}
 
 	computeValid() {
 		// valid if we have a condition, an ignore reason, and at least one value
-		return Boolean(this.configuration.condition) && this.configuration.values.length > 0 && Boolean(this.configuration.ignoreReason);
+		return (
+			Boolean(this.configuration.condition) &&
+			this.configuration.values.length > 0 &&
+			Boolean(this.configuration.ignoreReason)
+		);
 	}
 }
